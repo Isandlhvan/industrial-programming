4490b23c7662ba1ff4572670ca0dcbe82ac70d3b
Yegor <108461879+Isandlhvan@users.noreply.github.com>
2023-01-14 16:28:52 +0300

    Add files via upload

---
+++ Spring/Spring Data/target/classes/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO account VALUES (NULL, 'Hugo','Olimpio', 1000);
+INSERT INTO account VALUES (NULL, 'Valter','Scot',1000);
+INSERT INTO ACCOUNT VALUES (NULL, 'Pushkin','Dubrovskei',1000);
+INSERT INTO ACCOUNT VALUES (NULL, 'Lermontov','Hero of our times',1000);
+INSERT INTO ACCOUNT VALUES (NULL, 'Dostoevskei','Teenagers',1000);
+INSERT INTO ACCOUNT VALUES (NULL, 'Esenin','Sergei',1000);
+INSERT INTO ACCOUNT VALUES (NULL, 'Bunin','Something',1000);
